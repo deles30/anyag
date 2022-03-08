39c053d5744c3e4c803205d8d4d7830c040d912e
deles30 <100537754+deles30@users.noreply.github.com>
2022-03-08 22:32:47 +0100

    Update tombvesszo.c

--- tombvesszo.c
+++ tombvesszo.c
@@ -2,7 +2,7 @@
 
 void tomb_kiir(int meret, int tomb[]){
     for(int i=0; i<meret; i++){
-        if(i==9){
+        if(i==4){
             printf("%d ",tomb[i]);
             return;
         }
@@ -14,8 +14,8 @@ void tomb_kiir(int meret, int tomb[]){
 
 int main(){
 
-int szamok[10]={85,72,45,29,8,35,76,3,88,94};
-int meret = 10;
+int szamok[5]={4, 7, 34, 23, 67};
+int meret = 5;
 tomb_kiir(meret,szamok);
 
 return 0;
